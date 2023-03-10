024b730a40c7a475f8e6248862fc103d2dc3c52d
244564485 <244564485@qq.com>
2023-03-11 01:50:02 +0800

    20230311 01:50:02

--- test/cpp/rfrho/ExportZeroCurve.cc
+++ test/cpp/rfrho/ExportZeroCurve.cc
@@ -226,5 +226,31 @@ int main(int argc, char ** argv)
     exit(sSUCCESS);
 }
 
+void SplitString(const string &s, vector<string>&v, const string &c)
+{
+    string::size_type pos1, pos2;
+    pos2 = s.find(c);
+    pos1 = 0;
+
+    while(string::npos != pos2)
+    {
+        v.push_back(s.substr(pos1, pos2 - pos1));
+        pos1 = pos2 + c.size();
+        pos2 = s.find(c, pos1);
+    }
+
+    if(pos1 != s.length())
+        v.push_back(s.substr(pos1));
+
+}
+
+int WriteMD5File(const)
+
+
+
+
+
+
+
 
 
