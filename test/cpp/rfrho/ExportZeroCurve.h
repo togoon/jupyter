225c10c76838ca62083974198d436e9998d977cd
244564485 <244564485@qq.com>
2023-03-10 15:20:01 +0800

    20230310 15:20:01

--- test/cpp/rfrho/ExportZeroCurve.h
+++ test/cpp/rfrho/ExportZeroCurve.h
@@ -16,6 +16,50 @@
 
 #endif
 
+const char *sMCTYPE_short[] = 
+{
+    (char *) "        ",
+    (char *) "TWV     ",
+    (char *) "ZERO    ",
+    (char *) "PAR     ",
+    (char *) "DISC    ",
+    (char *) "FWD     ",
+    (char *) "CLOS    ",
+    (char *) "SPR     ",
+    (char *) "SIMP    ",
+    (char *) "FWWK    ",
+    (char *) "FWVL    ",
+    (char *) "TVOL    ",
+    (char *) "FXVL    ",
+    (char *) "CORR    ",
+    (char *) "FUTURE  ",
+    (char *) "BOND    ",
+    (char *) "INDEXSPR",
+    (char *) "MODELFAC",
+    (char *) "SEC_VOL ",
+    (char *) "SEC_SEC ",
+    (char *) "SEC_FX  ",
+    (char *) "SEC_FWD ",
+    (char *) "REV     ",
+    (char *) "VFACT   ",
+    (char *) "SHIFT   ",
+    (char *) "LISTEDOP",
+    (char *) "SMILE   ",
+    (char *) "        ",
+    (char *) "        ",
+    (char *) "        ",
+    (char *) "        ",
+    (char *) "        ",
+    (char *) "        ",
+    (char *) "        ",
+    (char *) "        ",
+    (char *) "        ",
+    (char *) "        ",
+    (char *) "        ",
+    (char *) "INDEXSPR",
+
+
+}
 
 
 
