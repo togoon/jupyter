dd31263c723d6935e504d76a21a200fe330b9f54
244564485 <244564485@qq.com>
2023-03-12 01:15:01 +0800

    20230312 01:15:01

--- test/cpp/rfrho/tongyeCustomerInterface2.cc
+++ test/cpp/rfrho/tongyeCustomerInterface2.cc
@@ -1,6 +1,16 @@
 // #include "tongyeCustomerInterface2.h"
+#include <string>
+
+using namespace std;
 
 int cUpdateCust(SU_ValueList *pSrcVL, string &outXml, string &errorXml, const string &inXml);
 int LogMessage(const string &message, const string &oust, const stirng &prefix, const string &action);
 int GetNameValue(const string &strNode, const string &strName, string &strVal);
+int SetNamevalue(string &strNode, string &strName, string &strVal);
+int DelNameNode(string &strNode, const string &strName);
+int DelNameLabel(const string &strName, string &strNode);
 
+int cUpdateCust(SU_ValueList *pSrcVL, string &outXml, string &errorXml, const string &inXml)
+{
+
+}
