0bbe6fefc1bcda83e782ff68a63f0929b01d810c
244564485 <244564485@qq.com>
2023-03-12 01:10:02 +0800

    20230312 01:10:01

---
+++ test/cpp/rfrho/tongyeCustomerInterface2.cc
@@ -0,0 +1,6 @@
+// #include "tongyeCustomerInterface2.h"
+
+int cUpdateCust(SU_ValueList *pSrcVL, string &outXml, string &errorXml, const string &inXml);
+int LogMessage(const string &message, const string &oust, const stirng &prefix, const string &action);
+int GetNameValue(const string &strNode, const string &strName, string &strVal);
+
