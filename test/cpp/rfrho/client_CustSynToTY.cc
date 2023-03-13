4f433c08dc7079ee1dedca87a69ed8f1e7fb324d
244564485 <244564485@qq.com>
2023-03-13 14:50:01 +0800

    20230313 14:50:01

--- test/cpp/rfrho/client_CustSynToTY.cc
+++ test/cpp/rfrho/client_CustSynToTY.cc
@@ -13,16 +13,21 @@
 SId; $
 */
 
+#include "client/stkpublicapi.h"
+#include "dbmappingapi/su_dbmapping.h"
+#include "sutools/su_cstring_slist.h"
+#include "xml/xml_entity_utils.h"
+#include <string>
+#include <fstream>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stringhelper.h>
+#include <boost/locale/encoding.hpp>
 
+using namespace std;
 
-
-
-
-
-
-
-
-
-
-
+int LogMessage(const string& message, const string& cust,const string& prefix, const string& action)
 
