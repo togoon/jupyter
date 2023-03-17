bd0c88ab80007181c9f7b58cc518bd21566f3de4
244564485 <244564485@qq.com>
2023-03-17 21:10:01 +0800

    20230317 21:10:01

---
+++ test/cpp/log/log.cc
@@ -0,0 +1,18 @@
+#include "log.h"
+
+LOG *LOG::Log = NULL;
+string LOG::logBuffer = "";
+HANDLE LOG::mFileHandle = INVAILD_HANDLE_VALUE;
+muter LOG::log_mutex;
+CRITICAL_SECTION LOG::criticalsection;
+int LOG::writtenSize = 0;
+
+LOG::LOG()
+{
+    init(LOG_LEVEL_NONE, LOG_TARGET_FILE);
+}
+
+void LOG::init(LOGLEVEL loglevel, LOGTARGET logtarget)
+{
+    setLogLevel(loglevel);
+}
