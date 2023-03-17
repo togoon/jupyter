502a232e0d91028e15cff08583623969564dfc11
244564485 <244564485@qq.com>
2023-03-17 20:50:01 +0800

    20230317 20:50:01

--- test/cpp/log/log.h
+++ test/cpp/log/log.h
@@ -29,16 +29,26 @@ enum LOGTARGET
 #define LOG_DEBUG(...)  LOG::writeLog(LOG_LEVEL_DEBUG, (unsigned char*)(FILENAME(__FILE__)), (unsigned char*)(__FUNCTION__), (int)(__LINE__), __VA_ARGS__)
 #define LOG_INFO(...)  LOG::writeLog(LOG_LEVEL_INFO, (unsigned char*)(FILENAME(__FILE__)), (unsigned char*)(__FUNCTION__), (int)(__LINE__), __VA_ARGS__)
 #define LOG_WARNING(...)  LOG::writeLog(LOG_LEVEL_WARNING, (unsigned char*)(FILENAME(__FILE__)), (unsigned char*)(__FUNCTION__), (int)(__LINE__), __VA_ARGS__)
+#define LOG_ERROR(...)  LOG::writeLog(LOG_LEVEL_ERROR, (unsigned char*)(FILENAME(__FILE__)), (unsigned char*)(__FUNCTION__), (int)(__LINE__), __VA_ARGS__)
 #define LOG_DEBUG(...)  LOG::writeLog(LOG_LEVEL_DEBUG, (unsigned char*)(FILENAME(__FILE__)), (unsigned char*)(__FUNCTION__), (int)(__LINE__), __VA_ARGS__)
-#define LOG_DEBUG(...)  LOG::writeLog(LOG_LEVEL_DEBUG, (unsigned char*)(FILENAME(__FILE__)), (unsigned char*)(__FUNCTION__), (int)(__LINE__), __VA_ARGS__)
 
+#define ENTER() LOG_INFO("enter")
+#define EXIT() LOG_INFO("exit")
+#define FAIL() LOG_INFO("fail")
 
+// 单个日志文件最大存储
+#define MAX_SIZE 2*1024*1024 //2M
 
-
-
-
-
-
-
-
-
+class LOG
+{
+public:
+    void init(LOGLEVEL loglevel, LOGTARGET logtarget);
+    void uninit();
+    int createFile();
+    static LOG *getInstance();
+    LOGLEVEL getLogLevel();
+    void setLogLevel(LOGLEVEL loglevel);
+    LOGTARGET getLogTarget();
+    void setLogTarget(LOGTARGET logtarget);
+    
+}
